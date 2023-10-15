85662f7fcf0c56a362a6428334d12e27740af850
Desiree Zarco <desireeannzarco7@gmail.com>
2023-10-15 01:19:03 -0700

    final final

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,11 +1,78 @@
-import React from "react";
-
+import React, { useState, useEffect, useRef } from "react";
+import { GiBigDiamondRing } from "react-icons/gi";
 
 const Home = () => {
+  const [timerDays, setTimerDays] = useState('00');
+  const [timerHours, setTimerHours] = useState('00');
+  const [timerMinutes, setTimerMinutes] = useState('00');
+  const [timerSeconds, setTimerSeconds] = useState('00');
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const countdownDate = new Date('February 4, 2024 00:00:00').getTime();
+
+    interval = setInterval(() => {
+      const now = new Date ().getTime();
+      const distance = countdownDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        //stop timer
+        clearInterval(interval.current)
+      }else {
+        //update timer
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    }, 1000)
+  }
+
+useEffect(() => {
+  startTimer();
+  return() => {
+    clearInterval(interval.current);
+  }
+
+})
 
   return (
     <div className="home">
-      <h3>Mona & Dixon's Wedding</h3>
+    <section className="timer-container">
+      <section className="timer">
+        <div>
+        <span  className="timer-icon"></span>
+          <h1>Countdown until Wedding Day!</h1>
+        </div>
+        <div>
+          <section>
+            <p>{timerDays}</p>
+            <p><small>Days</small></p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerHours}</p>
+            <p><small>Hours</small></p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerMinutes}</p>
+            <p><small>Minutes</small></p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerSeconds}</p>
+            <p><small>Seconds</small></p>
+          </section>
+        </div>
+      </section>
+    </section>
     </div>
   );
 }
